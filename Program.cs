45603e98c89223b89ae055b9ec4f5162615c6ef2
misthios997 <misthios997@gmail.com>
2026-01-17 14:07:32 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Windows.Forms;
 
+namespace GalaxyPCHost
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                HostManager.Disconnect();
+            };
+
+            Application.Run(new MainForm());
+        }
+    }
+}
