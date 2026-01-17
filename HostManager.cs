0e262e10c853eb6b80c532ed5053d719b1103173
misthios997 <misthios997@gmail.com>
2026-01-17 13:06:56 +0700

    Create HostManager.cs

---
+++ HostManager.cs
@@ -0,0 +1,30 @@
+using System.IO;
+
+namespace GalaxyPCHost
+{
+    public static class HostManager
+    {
+        static string hosts = @"C:\Windows\System32\drivers\etc\hosts";
+        static string backup = @"C:\Windows\System32\drivers\etc\hosts.bak";
+
+        static string gtps =
+@"91.134.85.13 growtopia1.com
+91.134.85.13 growtopia2.com
+91.134.85.13 www.growtopia1.com
+91.134.85.13 www.growtopia2.com";
+
+        public static void Connect()
+        {
+            if (!File.Exists(backup))
+                File.Copy(hosts, backup);
+
+            File.AppendAllText(hosts, "\n" + gtps);
+        }
+
+        public static void Disconnect()
+        {
+            if (File.Exists(backup))
+                File.Copy(backup, hosts, true);
+        }
+    }
+}
